cc516a276eea9ce804772fdc969accd65d9ea73f
AdamTheisen <adamtheisen@hotmail.com>
2022-08-18 11:38:36 -0500

    Updating data

--- sps30/plot_sps.py
+++ sps30/plot_sps.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+myFmt = mdates.DateFormatter('%m/%d-%H')
 
-file = '/home/theisen/Code/Pi-Projects/data/sps30/20220815.txt'
+file = '/home/theisen/Code/Pi-Projects/data/sps30/20220818.txt'
 
 names = ['time', 'pc_0_5', 'pc_1_0', 'pc_2_5', 'pc_4_0', 'pc_10_0',
          'mc_1_0', 'mc_2_5', 'mc_4_0', 'mc_10_0','particle_size',
          'particle_size_unit', 'particle_count_unit', 'mass_density_unit']
-df = pd.read_csv(file, index_col=0, names=names)
-df['mc_2_5'].plot()
+df = pd.read_csv(file, index_col=0, names=names, parse_dates=True)
+ax = df['mc_2_5'].plot()
+ax.xaxis.set_major_formatter(myFmt)
 plt.show()
