e4dd9f5a71e11d2f54b43b47a17646a88d1d4c73
Adam Theisen <atheisen@anl.gov>
2022-09-15 14:52:24 -0500

    ENH: Adding documentation

--- sps30/compare_sps.py
+++ sps30/compare_sps.py
@@ -11,26 +11,28 @@ names = ['time', 'pc_0_5', 'pc_1_0', 'pc_2_5', 'pc_4_0', 'pc_10_0',
          'mc_1_0', 'mc_2_5', 'mc_4_0', 'mc_10_0','particle_size',
          'particle_size_unit', 'particle_count_unit', 'mass_density_unit']
 
-
-#df = pd.read_csv(files[0], index_col=0, names=names, parse_dates=True)
-#obj = df.to_xarray()
+# Open the SP file
 obj = act.io.csvfiles.read_csv(files, column_names=names, parse_dates=True, index_col=0, ignore_index=False, engine='c')
 
-#time = obj['time'].values
-#time = [t + np.timedelta64(5, 'h') for t in time]
-#obj = obj.assign_coords(time=time) 
+# Sort the times to be sure there aren't any issues
 obj = obj.sortby('time')
 
+# Average to 1 hour
 obj = obj.resample(time='1H').mean()
+
+# Set Lat/lon of the system
 obj['lat'] = 41.96252
 obj['lon'] = -88.12377
-#da = obj['mc_2_5'].rolling(time=10).mean()
-#obj['mc_2_5'] = da
 
+# Needs an AIRNOW_API environment variable set with your TOKEN
+# sign up here: https://docs.airnowapi.org/
 token = os.getenv('AIRNOW_API')
+
+# Set Lat/lon box for EPA sites
 latlon = '-88.3,41.7,-87.9,42.3'
+
+# Get EPA data
 obj2 = act.discovery.get_airnow.get_airnow_bounded_obs(token, '2022-09-01T00', '2022-09-11T23', latlon, parameters='PM25')
-#obj2 = obj2.squeeze(dim='sites', drop=False)
 
 display = act.plotting.TimeSeriesDisplay({'sps30': obj, 'epa': obj2})
 display.plot('mc_2_5', set_title='PM2.5', dsname='sps30', label='SPS30 2.5')
